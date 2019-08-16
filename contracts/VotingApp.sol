1b04199066d7d220d2172a15878c291bb1e6ede1
rahulj3716 <rahulj3716@gmail.com>
2019-08-17 02:17:39 +0530

    edited .json and .sol files

--- contracts/VotingApp.sol
+++ contracts/VotingApp.sol
@@ -10,14 +10,14 @@ contract VotingApp
     string public Party;
     string public CandidateName;
     string public Region;
-    StateType public state;
+    StateType public State;
 
     constructor (string memory party, string memory candidateName, string memory region) public {
         EVM = msg.sender;
         Party = party;
         CandidateName = candidateName;
         Region = region;
-        state = StateType.Voted;
+        State = StateType.Voted;
     }
 
 }
